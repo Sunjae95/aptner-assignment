1ee337623da064d5d972af26a01c5ccb3a8dae5e
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 04:40:43 +0900

    ✨ 메인페이지 구조 설계

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <main>
+      <ul>
+        <li>
+          <Link href={'/'}>사용자 검색</Link>
+        </li>
+        <li>
+          <Link href={'?tab=bookmark'}>북마크 사용자</Link>
+        </li>
+      </ul>
+      <input placeholder="사용자를 검색해주세요." />
+      <button>검색</button>
+      <ol>
+        <li>유저1</li>
+        <li>유저2</li>
+        <li>유저3</li>
+      </ol>
+    </main>
+  );
 }
