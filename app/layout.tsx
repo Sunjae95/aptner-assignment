929fb620f7b1f0b41b9af6f4821207ac4d11be75
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 04:32:18 +0900

    📦 sass setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 
+import '@/styles/reset.scss';
+
 export const metadata: Metadata = {
   title: 'Aptner-assignment',
   description: 'GitHub 유저를 검색하고 해당 유저를 북마크 서비스',
