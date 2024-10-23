bbdd4bfeea448ff5bc754e2c57872dc5e589c499
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 04:21:09 +0900

    📦 eslint, prettier setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Aptner-assignment",
-  description: "GitHub 유저를 검색하고 해당 유저를 북마크 서비스",
+  title: 'Aptner-assignment',
+  description: 'GitHub 유저를 검색하고 해당 유저를 북마크 서비스',
 };
 
 export default function RootLayout({
