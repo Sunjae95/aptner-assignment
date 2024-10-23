b5cbe55dda3a2370eedaeb427fc02f5e492a5fcb
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 04:12:00 +0900

    🎉 next project setting

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Aptner-assignment",
+  description: "GitHub 유저를 검색하고 해당 유저를 북마크 서비스",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
