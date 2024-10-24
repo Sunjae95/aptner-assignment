71096af1fc929bdd4b882c0c3692dd606ea44bb4
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 22:03:59 +0900

    ✨ github user api 타입정의

---
+++ types/user.ts
@@ -0,0 +1,19 @@
+export interface UserType {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string | null;
+  html_url: string;
+  id: number;
+  node_id: string;
+  login: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  site_admin: boolean;
+  type: string;
+  url: string;
+  score: number;
+}
