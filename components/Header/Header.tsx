3503923536a0f86edc45d520d949989319ddf57d
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 18:20:37 +0900

    ✨ Header 컴포넌트 구현

---
+++ components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link, { LinkProps } from 'next/link';
+import Image from 'next/image';
+
+import styles from './Header.module.scss';
+
+interface HeaderItem extends LinkProps {
+  label: string;
+}
+
+interface HeaderProps {
+  items: HeaderItem[];
+}
+
+export default function Header({ items }: HeaderProps) {
+  return (
+    <header className={styles.container}>
+      <Image alt="logo" src={'/assets/logo.png'} width={100} height={42} priority />
+      <ul className={styles.wrapper}>
+        {items.map(({ label, ...linkProps }) => (
+          <li key={`tab-${linkProps.href}`} className={styles.item}>
+            <Link {...linkProps} className={styles.link}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+}
