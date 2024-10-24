bf12315bf3202f73c91483b95f6db0fd9280b52b
SeonjaeLee <tjswo9502@gmail.com>
2024-10-24 21:02:16 +0900

    ✨ Card 컴포넌트 구현

---
+++ components/Card/Card.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+
+import styles from './Card.module.scss';
+
+export interface CardType {
+  id: number;
+  login: string;
+  avatar_url: string;
+  html_url: string;
+}
+
+export interface CardProps extends CardType {
+  isBookmark: boolean;
+  onClick: () => void;
+}
+
+export default function Card({ login, avatar_url, html_url, isBookmark, onClick }: CardProps) {
+  return (
+    <li className={styles.container}>
+      <Image src={avatar_url} alt={`${login}-avatar`} width={24} height={24} className={styles.image} />
+      <div className={styles.wrapper}>
+        <p className={styles.login}>{login}</p>
+        <p className={styles.html_url}>{html_url}</p>
+      </div>
+      <div className={styles.bookmark_wrapper} onClick={onClick}>
+        {isBookmark ? (
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M5 2H19C19.2652 2 19.5196 2.10536 19.7071 2.29289C19.8946 2.48043 20 2.73478 20 3V22.143C20.0001 22.2324 19.9763 22.3202 19.9309 22.3973C19.8855 22.4743 19.8204 22.5378 19.7421 22.5811C19.6639 22.6244 19.5755 22.6459 19.4861 22.6434C19.3968 22.641 19.3097 22.6146 19.234 22.567L12 18.03L4.766 22.566C4.69037 22.6135 4.60339 22.6399 4.5141 22.6424C4.42482 22.6449 4.33649 22.6235 4.2583 22.5803C4.1801 22.5371 4.11491 22.4738 4.06948 22.3969C4.02406 22.32 4.00007 22.2323 4 22.143V3C4 2.73478 4.10536 2.48043 4.29289 2.29289C4.48043 2.10536 4.73478 2 5 2Z"
+              fill="black"
+            />
+          </svg>
+        ) : (
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M5 2H19C19.2652 2 19.5196 2.10536 19.7071 2.29289C19.8946 2.48043 20 2.73478 20 3V22.143C20.0001 22.2324 19.9763 22.3202 19.9309 22.3973C19.8855 22.4743 19.8204 22.5378 19.7421 22.5811C19.6639 22.6244 19.5755 22.6459 19.4861 22.6434C19.3968 22.641 19.3097 22.6146 19.234 22.567L12 18.03L4.766 22.566C4.69037 22.6135 4.60339 22.6399 4.5141 22.6424C4.42482 22.6449 4.33649 22.6235 4.2583 22.5803C4.1801 22.5371 4.11491 22.4738 4.06948 22.3969C4.02406 22.32 4.00007 22.2323 4 22.143V3C4 2.73478 4.10536 2.48043 4.29289 2.29289C4.48043 2.10536 4.73478 2 5 2ZM18 4H6V19.432L12 15.671L18 19.432V4Z"
+              fill="black"
+            />
+          </svg>
+        )}
+      </div>
+    </li>
+  );
+}
